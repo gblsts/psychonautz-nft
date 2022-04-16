53be4b51606af1373f89aaf5787522dcc6544647
jcarlos <jcarlos@gblsts.com>
2022-04-16 00:01:09 -0500

    [PSY-03] Lock compiler version to 0.8.9

--- contracts/Psychonautz.sol
+++ contracts/Psychonautz.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
